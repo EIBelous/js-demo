0d4ed01a5f5552137a424057536b724b49ff3485
EIBelous <evgen.i.bilous@gmail.com>
2022-04-10 18:47:21 +0300

    nested function

--- home.js
+++ home.js
@@ -1,5 +1,17 @@
-let i=6;
-while(i>0){
-    console.log(i)
-    i--
+
+let key=42
+
+function secretCode(value){
+
+    let keyGenerator=function(){
+        let key=22
+        console.log('in keyGenerator', key)
+        return key
+    }
+let code=keyGenerator()
+    console.log('in secretCode', key)
+    return code
+
 }
+
+showMessage(secretCode(2))
