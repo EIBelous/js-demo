8b2db201cbb78bf1d8665a8bd87cbca4917776fb
EIBelous <evgen.i.bilous@gmail.com>
2022-04-10 19:06:28 +0300

    using function to modify Web

--- home.js
+++ home.js
@@ -1,17 +1,5 @@
 
-let key=42
 
-function secretCode(value){
 
-    let keyGenerator=function(){
-        let key=22
-        console.log('in keyGenerator', key)
-        return key
-    }
-let code=keyGenerator()
-    console.log('in secretCode', key)
-    return code
 
-}
-
-showMessage(secretCode(2))
+chengepercent(30)
--- utils.js
+++ utils.js
@@ -1,5 +1,8 @@
 function showMessage(message) {
     document.getElementById('message1').textContent = message;
-    
-
 }
+
+function chengepercent(percentage){
+document.getElementById('percent-off').textContent=
+percentage+' OFF'}
+
