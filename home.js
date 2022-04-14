b4b2ebb455f9f684c0e60e57daa5e1711967c98c
EIBelous <evgen.i.bilous@gmail.com>
2022-04-14 18:39:44 +0300

    toggle hide class

--- home.js
+++ home.js
@@ -1,4 +1,14 @@
 const button=document.getElementById('see-review')
 button.addEventListener('click', function(){
-    console.log('click')
+   const rev=document.getElementById("review")
+
+   if (rev.classList.contains("d-none")){
+   rev.classList.remove("d-none")
+   button.textContent='Hide review'
+}
+   else{
+       rev.classList.add("d-none")
+       
+       button.textContent="SEE REview"
+   }
 })
